cde9450253cb70a34035f2dda621225a68bd52b4
LaCharogne <romuald_senigallia@hotmail.com>
2020-01-09 11:43:32 +0100

    Update UserCheck.java

--- SangRancune_Lib/src/main/java/com/MarSen/app/SangRancune_Lib/UserCheck.java
+++ SangRancune_Lib/src/main/java/com/MarSen/app/SangRancune_Lib/UserCheck.java
@@ -1,7 +1,15 @@
 package com.MarSen.app.SangRancune_Lib;
 
+/*
+ * Vérification du mot de passe et du login.
+ * Si login faux, alerte
+ * Si password faux, alerte + proposition de réinitialisation
+ * => prévoir une classe à part pour la réinitialisation password
+ */
+
 public class UserCheck {
 String login;
 String password;
 Boolean canConnect;
 }
+
