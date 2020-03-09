2e5cf9ea59668abc9e14e133d9e1410efbfbb15b
LaCharogne <romuald_senigallia@hotmail.com>
2020-03-09 12:30:21 +0100

    Actualisation

--- Messenger/src/Lib/Logged.java
+++
@@ -1,9 +0,0 @@
-package Lib;
-
-import java.io.Serializable;
-
-public class Logged implements Serializable{
-	public String login;
-	public boolean logged;
-
-}
--- Messenger/src/Lib/UserCon.java
+++
@@ -1,9 +0,0 @@
-package Lib;
-
-import java.io.Serializable;
-
-public class UserCon implements Serializable {
-public String login;
-public String password;
-public boolean canConnect;
-}
